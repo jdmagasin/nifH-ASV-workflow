1f6955fa5340931f8f9f5e8269220bb4e0787f3f
Jonathan Magasin <jmagasin@ucsc.edu>
2024-03-15 12:32:01 -0700

    Small fix to consensus_id:  Prevent 'unknownnan' and subcluster if defined, if not then subcluster if defined, otherwise 'unknown'.

--- AnnotateAuids/scripts/make_consensus_taxonomy.py
+++ AnnotateAuids/scripts/make_consensus_taxonomy.py
@@ -66,11 +66,11 @@ def clean_columns(df):
     Clean up columns in the DataFrame.
     """
     if df is not None:
-        # Clean up columns
-        df["subcluster"] = df["subcluster"].astype(str)
+        # Clean up columns.  Make sub/cluster strings with "" for unknown.
+        df["subcluster"] = df["subcluster"].fillna("")
         df["cluster"] = df["cluster"].apply(
             lambda x: str(object=int(x)) if not pd.isna(x) else x
-        )
+        ).fillna("")
         # Make new ID columns
         df["MarineDiazo.id"] = (
             df["MarineDiazo.description"] + ";" + df["MarineDiazo.subject"]
@@ -102,9 +102,9 @@ def make_consensus_id(df, min_pid_genome879=97.0):
             .fillna(
                 df.apply(
                     lambda row: (
-                        "unknown" + row["cluster"]
-                        if (row["cluster"] == "3" or row["cluster"] == "4")
-                        else "unknown" + row["subcluster"]
+                        "unknown" + row["subcluster"] if (not row["subcluster"] == "")
+                        else "unknown" + row["cluster"] if (not row["cluster"] == "")
+                        else "unknown"
                     ),
                     axis=1,
                 )
