9c5cd58840033b0c1b5edba4380cb1b77be6171d
mo <mikemojr@gmail.com>
2024-05-05 16:24:31 -0700

    Clean up documentation of the make_primary_taxon_id.py

--- AnnotateAuids/scripts/make_primary_taxon_id.py
+++ AnnotateAuids/scripts/make_primary_taxon_id.py
@@ -1,20 +1,21 @@
+#!/usr/bin/env python3
 """
 make_primary_taxon_id.py
 
-This script is part of the AnnotateAuids stage of the nifH ASV workflow. It is managed by a Makefile and depends on the merged annotation table (auids.annot.tsv) created by this stage. 
+This module adds a primary_id column to an annotation table and is part of the AnnotateAuids stage of the nifH-ASV-workflow. 
 
 Overview:
     - reads an input annotation table tsv file
     - cleans up columns in the DataFrame
     - creates a primary taxonomy ID -> primary_id
     - writes the updated annotation table with new column primary_id added
-A new column "primary_id" is added to the input table (auids.annot.tsv), representing the most informative taxonomic ID aggregated across the merged annotation table supplied and new thresholds passed to this script. This ID should be used in any discussion of the AUID. Each AUID is assigned a new ID hierarchically. Oligotypes for UCYN-A are always used when available. NCD/cyano ID are consider next followed by Genome879 tax ID that pass new threshold supplied to the script (default is 97.0%). If none of these specific ID are available, the more general nifH cluster is used. 
+make_primary_taxon_id.py is managed by a Makefile and depends on the merged annotation table (auids.annot.tsv) created by the AnnotateAuids stage of the nifH-ASV-workflow. A new column "primary_id" is added to the input annotation table, representing the most informative taxonomic ID aggregated across the merged annotation table supplied. This 'primary ID' is the suggested way to refer to each AUID and is assigned hierarchically. Oligotypes for UCYN-A are always used when available. NCD/cyano ID are consider next followed by Genome879 tax ID if it passes a new threshold supplied to the module (default is 97.0%). A new threshold is considered here because the original percent identity used was low to improve the chances of finding a match. However, for analysis purposes, a more refined value is preferred. If none of these specific ID are assigned, the more general nifH cluster is used.
 
 A main function executes and manages the entire process of the script. Below is a breakdown of its functionality:
 
     1. Parse Command-Line Arguments:
-        - Uses argparse to parse the command-line arguments provided by the user.
-        - Requires the input path to the annotation table, output path for the updated table, and optionally, the minimum threshold percentage identity for Genome879 ID.
+        - Uses argparse module to parse the command-line arguments provided by the user.
+        - Requires the input path to the annotation table, output path for the updated table, and optionally, the minimum threshold percentage identity for Genome879 ID. 97% is used by default.
     
     2. Read Input Data:
         - Calls the 'read_tsv()' function to read the input annotation table into a pandas DataFrame.
@@ -24,21 +25,22 @@ A main function executes and manages the entire process of the script. Below is 
         - Invokes the 'clean_columns()' function to perform necessary cleanup operations on the DataFrame.
         - Ensures that the DataFrame is properly formatted and ready for primary ID generation.
 
-    4. Generate Consensus IDs:
+    4. Generate Primary IDs:
         - Utilizes the 'make_primary_id()' function to create primary taxonomy IDs based on specified criteria.
-        - Consensus IDs are assigned hierarchically, considering various taxonomic IDs and thresholds.
+        - Primary IDs are assigned hierarchically, considering various taxonomic IDs and thresholds.
 
     5. Write Output Data:
         - Calls the 'write_tsv()' function to write the updated DataFrame with primary IDs to a TSV file.
         - Provides feedback to the user upon successful completion or error during file writing.
 
+
 Usage:
-    python make_primary_taxon_id.py <path_to_input_table> <path_to_output_table> [--min_pid_genome879 <threshold_pid>] [-h,--help]
+    python3 make_primary_taxon_id.py <path_to_input_table> <path_to_output_table> [--min_pid_genome879 <threshold_pid>] [-h,--help]
 
 Arguments:
-    annotation_table: Path to the input annotation table.
+    annotation_table: Path to the input annotation table (as tsv).
     output_table: Path to the output annotation table with primary ID added.
-    --min_pid_genome879: Minimum threshold percentage identity to consider Genome879.id in primary ID. If not provided, default value is 97.0.
+    --min_pid_genome879: Minimum threshold percentage identity to consider. Genome879.id in primary ID. If not provided, default value is 97.0.
 
 Dependencies:
 - pandas
@@ -54,6 +56,11 @@ import sys
 import pandas as pd
 from argparse import RawTextHelpFormatter
 
+__author__ = "Michael (Mo) Morando"
+__copyright__ = "Copyright 2023"
+__maintainer__ = "Michael (Mo) Morando"
+__email__ = "mikemo@gmail.com"
+__status__ = "Stable"
 
 # Get the full path to the currently running script
 script_path: str = __file__
@@ -80,13 +87,13 @@ Overview of {script_name}:
     - cleans up columns in the DataFrame
     - creates a primary taxonomy ID -> primary_id
     - writes the updated annotation table with new column primary_id added
-A new column "primary_id" is added to the input table (auids.annot.tsv), representing the most informative taxonomic ID aggregated across the merged annotation table supplied and new thresholds passed to this script. This ID should be used in any discussion of the AUID. Each AUID is assigned a new ID hierarchically. Oligotypes for UCYN-A are always used when available. NCD/cyano ID are consider next followed by Genome879 tax ID that pass new threshold supplied to the script (default is 97.0%). If none of these specific ID are available, the more general nifH cluster is used. 
+This module is managed by a Makefile and depends on the merged annotation table (auids.annot.tsv) created by the AnnotateAuids stage of the nifH-ASV-workflow. A new column "primary_id" is added to the input annotation table, representing the most informative taxonomic ID aggregated across the merged annotation table supplied. This 'primary ID' is the suggested way to refer to each AUID and is assigned hierarchically. Oligotypes for UCYN-A are always used when available. NCD/cyano ID are consider next followed by Genome879 tax ID if it passes a new threshold supplied to the module (default is 97.0%). A new threshold is considered here because the original percent identity used was low to improve the chances of finding a match. However, for analysis purposes, a more refined value is preferred. If none of these specific ID are assigned, the more general nifH cluster is used.
 
 	A main function executes and manages the entire process of the script. Below is a breakdown of its functionality:
 
 	1. Parse Command-Line Arguments:
-	- Uses argparse to parse the command-line arguments provided by the user.
-	- Requires the input path to the annotation table, output path for the updated table, and optionally, the minimum threshold percentage identity for Genome879 ID.
+	- Uses argparse module to parse the command-line arguments provided by the user.
+    - Requires the input path to the annotation table, output path for the updated table, and optionally, the minimum threshold percentage identity for Genome879 ID. 97% is used by default.
 
 	2. Read Input Data:
 	- Calls the 'read_tsv()' function to read the input annotation table into a pandas DataFrame.
@@ -96,9 +103,9 @@ A new column "primary_id" is added to the input table (auids.annot.tsv), represe
 	- Invokes the 'clean_columns()' function to perform necessary cleanup operations on the DataFrame.
 	- Ensures that the DataFrame is properly formatted and ready for primary ID generation.
 
-	4. Generate Consensus IDs:
+	4. Generate Primary IDs:
 	- Utilizes the 'make_primary_id()' function to create primary taxonomy IDs based on specified criteria.
-	- Consensus IDs are assigned hierarchically, considering various taxonomic IDs and thresholds.
+	- Primary IDs are assigned hierarchically, considering various taxonomic IDs and thresholds.
 
 	5. Write Output Data:
 	- Calls the 'write_tsv()' function to write the updated DataFrame with primary IDs to a TSV file.
@@ -110,7 +117,7 @@ A new column "primary_id" is added to the input table (auids.annot.tsv), represe
     )
     parser.add_argument(
         "annotation_table",
-        help="Path to the input annotation table",
+        help="Path to the input annotation table as tsv",
     )
     parser.add_argument(
         "output_table",
@@ -120,7 +127,7 @@ A new column "primary_id" is added to the input table (auids.annot.tsv), represe
         "--min_pid_genome879",
         type=float,
         default=97.0,
-        help="Minimum threshold pid to consider Genome879.id in primary ID",
+        help="Minimum threshold pid to consider Genome879.id in primary ID. Default is 97.0 PID.",
     )
 
     return parser
@@ -134,7 +141,7 @@ def read_tsv(annotation_table: str) -> pd.DataFrame:
         annotation_table (str): Path to the input annotation table.
 
     Returns:
-        pd.DataFrame: The DataFrame read from the input file
+        pd.DataFrame: The DataFrame read from the input file.
     """
     try:
         print(
@@ -145,7 +152,6 @@ def read_tsv(annotation_table: str) -> pd.DataFrame:
             filepath_or_buffer=annotation_table,
             sep="\t",
         )
-
         return df
 
     except FileNotFoundError:
@@ -155,13 +161,13 @@ def read_tsv(annotation_table: str) -> pd.DataFrame:
         print(f"Error: DataFrame: '{annotation_table}' is empty")
         sys.exit(1)
     except pd.errors.ParserError:
-        print(f"Error: There was an error parsing the csv file: '{annotation_table}'.")
+        print(f"Error: There was an error parsing the TSV file: '{annotation_table}'.")
         sys.exit(1)
 
 
 def clean_columns(df: pd.DataFrame) -> pd.DataFrame:
     """
-    Clean up columns in the DataFrame to work with primary ID function.
+    Clean up columns in the DataFrame to work with primary ID function -> make_primary_id().
 
     Args:
         df (pd.DataFrame): DataFrame containing the input data.
@@ -232,7 +238,7 @@ def clean_columns(df: pd.DataFrame) -> pd.DataFrame:
         print(f"Error: {e}")
         sys.exit(1)
     except Exception as e:
-        print(f"Error: {e}")
+        print(f"An unexpected error occurred: {e}")
         sys.exit(1)
 
 
@@ -245,7 +251,7 @@ def make_primary_id(
 
     Args:
         df (pd.DataFrame): DataFrame containing the input data.
-        min_pid_genome879 (float): Minimum threshold percentage identity to consider Genome879.id in primary ID.
+        min_pid_genome879 (float): Minimum threshold percentage identity to consider Genome879.id in primary ID. 97.0% is used by default.
 
     Returns:
         pd.DataFrame: The DataFrame with the primary ID added.
@@ -347,14 +353,13 @@ def main():
         if args.annotation_table is None or args.output_table is None:
             parser.print_help()
             sys.exit()
-        # return
 
     except argparse.ArgumentError as e:
         print(f"Error: {e}")
         print("Failed to parse command-line arguments.")
         sys.exit(5)
     except Exception as e:
-        print(f"Error: {e}")
+        print(f"An unexpected error occurred: {e}")
         print(f"Failed to execute script: '{script_name}'.")
         sys.exit(11)
 
